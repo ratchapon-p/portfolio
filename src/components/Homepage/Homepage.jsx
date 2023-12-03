c949bca441b10522423b97fef4c78059ed212e98
unknown <ratchapon.p01@gmail.com>
2023-12-03 22:56:08 +0700

    test

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,51 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import './Homepage.css'
-import { ChevronDoubleDownIcon  } from "@heroicons/react/20/solid";
-import profile from '../../assets/profile.png'
-import post from '../../assets/homepage_pic.png'
-
+import React, { useEffect, useState } from "react";
+import "./Homepage.css";
+import { ChevronDoubleDownIcon } from "@heroicons/react/20/solid";
+import profile from "../../assets/profile.png";
+import post from "../../assets/homepage_pic.png";
 
 const Homepage = () => {
-
   const [useScroll, setUseScroll] = useState(false);
 
-  const scrollPage = () =>{
-    
-  }
+  const scrollPage = () => {};
 
-  
- 
   // useEffect(()=>{
   //   window.onscroll
   // })
 
-  
-  
   return (
-    <div className='intropage'>
-          <div className='homepage-main'>
-      <div className='display-text'>
-        <h2 id='welcome-typing' className='welcome'>Hello I'm James.</h2><h2 className='underline'> _</h2>
-      </div>
-      
-      <div className='scroll-down'>Scroll down to view profile<ChevronDoubleDownIcon className='scroll-down-icon'/></div>
+    // <div className="intropage">
+    //   <div className="homepage-main">
+    //     <div className="display-text">
+    //       <h2 id="welcome-typing" className="welcome">
+    //         Hello I'm James.
+    //       </h2>
+    //       <h2 className="underline"> _</h2>
+    //     </div>
 
+    //     <div className="scroll-down">
+    //       Scroll down to view profile
+    //       <ChevronDoubleDownIcon className="scroll-down-icon" />
+    //     </div>
+    //   </div>
 
-    </div>
-    
-    <div className='introduct2me'>
-        <img src={profile} className='intropage-profile'/>
-        <div className='intropage-description'>Me?</div>
-      </div>
-    
-    <div className='next2page'>
-        <img src={profile}/>
-        <div>Me?</div>
-      </div>
-      
-    </div>
+    //   <div className="introduct2me">
+    //     <img src={profile} className="intropage-profile" />
+    //     <div className="intropage-description">Me?</div>
+    //   </div>
 
-  )
-}
+    //   <div className="next2page">
+    //     <img src={profile} />
+    //     <div>Me?</div>
+    //   </div>
+    // </div>
+    <div class="container">
+    <div class="section"></div>
+    <div class="section"></div>
+    <div class="section"></div>
+    <div class="section"></div>
+    <div class="section"></div>
+</div>
+  );
+};
 
-export default Homepage
+export default Homepage;
